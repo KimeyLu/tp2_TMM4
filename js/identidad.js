c0d45054b0156445aa92ffec06d3e3c884eab562
KimeyLu <128751189+KimeyLu@users.noreply.github.com>
2026-09-06 12:13:51 -0300

    Identidad grafico

--- js/identidad.js
+++ js/identidad.js
@@ -76,7 +76,7 @@ const sketchIdentidad = (p) => {
     p.push();
     p.noStroke();
     p.fill(bgColor);
-    p.rect(0, 0, p.width, p.height/3); //mamá soy multimedia
+    p.rect(0, 0, p.width, p.height/2);
     p.pop();
     ShapesWithIdentityMovement();
     
